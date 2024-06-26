01a965c7843e7334f299829d77863fc0fad917b4
ay0x <85720179+ay0x@users.noreply.github.com>
2024-06-26 21:05:53 +0200

    feat: Test upload file handling
    Test file handling and processing

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UploadFileForm
+from .models import UploadFile
 
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('uploaded_files')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+def uploaded_files(request):
+    files = UploadFile.objects.all()
+    return render(request, 'uploaded_files.html', {'files': files})
