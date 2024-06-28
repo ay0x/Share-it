49dfca4b18bd3f47e995d0e890fdc3eced4b75f7
ay0x <85720179+ay0x@users.noreply.github.com>
2024-06-28 13:44:40 +0200

    style: Remove commented code
    Removed commented code

--- upload/urls.py
+++ upload/urls.py
@@ -1,11 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('upload/', views.upload_file, name='upload_file'),
-#     path('uploaded_files/', views.uploaded_files, name='uploaded_files'),
-# ]
-# urls.py
 from django.urls import path
 from . import views
 
--- upload/views.py
+++ upload/views.py
@@ -1,26 +1,3 @@
-# from django.shortcuts import render, redirect
-# from django.http import HttpResponse
-# from .forms import UploadFileForm
-# from .models import UploadFile
-
-# def upload_file(request):
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             files = request.FILES.getlist('files')
-#             for file in files:
-#                 uploaded_file = UploadFile(file=file)
-#                 uploaded_file.save()
-#             return redirect('uploaded_files')
-#     else:
-#         form = UploadFileForm()
-#     return render(request, 'upload.html', {'form': form})
-
-# def uploaded_files(request):
-#     files = UploadFile.objects.all()
-#     return render(request, 'uploaded_files.html', {'files': files})
-# views.py
-
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .forms import UploadFileForm
