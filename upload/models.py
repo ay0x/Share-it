af427ff77f7712d979662a50203b3282752466f3
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-01 13:28:25 +0200

    feat: Update upload model to generate token for download link
    Create utility function to generate random token for upload model, the random token will be used for download link

--- upload/models.py
+++ upload/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from shareit.my_utils import generate_token
 
 class UploadFile(models.Model):
     file = models.FileField(upload_to='uploads/')
     file_name = models.CharField(max_length=255)
     file_size = models.PositiveBigIntegerField()
     upload_date = models.DateTimeField(auto_now_add=True)
-    expiration_date = models.DateTimeField(null=True, blank=True)
+    expiration_date = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=24))
     upload_by = models.CharField(max_length=255)
-    email = models.EmailField(null=True, blank=True)
-    name = models.CharField(max_length=255, null=True, blank=True)
+    download_link = models.CharField(max_length=7, default=generate_token(), editable=False, unique=True)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
@@ -20,4 +20,5 @@ class UploadFile(models.Model):
             self.file_name = self.file.name
         if not self.file_size:
             self.file_size = self.file.size
+        self.expiration_date = timezone.now() + timezone.timedelta(hours=24)
         super().save(*args, **kwargs)
