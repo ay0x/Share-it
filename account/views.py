ddaca6c015e80a3d04ac37e7e1ff0b1ede0018cf
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-03 22:48:33 +0200

    feat: Add views.py to handle requests for 'account' app
    Added views for account app requests

--- account/views.py
+++ account/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
+from django.contrib import messages
+from .forms import ProfileForm
 
-def index(request):
-    return render(request, 'pages/home.html') 
+@login_required(login_url='/login/')
+def dashboard(request):
+    return render(request, 'pages/home.html')
+
+@login_required(login_url='/login/')
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, 'account/profile.html', {'form': form})
+
+@login_required(login_url='/login/')
+def history(request):
+    user_files = request.user.uploadfile_set.all()
+    return render(request, 'account/history.html', {'user_files': user_files})
+
+@login_required(login_url='/login/')
+def logout(request):
+    auth_logout(request)
+    messages.success(request, 'You have successfully logged out.')
+    return redirect('upload_file')
