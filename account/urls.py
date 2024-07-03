7dbb33d221116a5dbcc7f67fe98b9c667c2b1389
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-03 22:36:59 +0200

    feat: Add URL patterns for routing
    Added URL patterns to route the appropriate request to the right views

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/', views.index, name='index')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/', views.profile, name='profile'),
+    path('history/', views.history, name='history'),
+    path('logout/', views.logout, name='logout'),
 ]
