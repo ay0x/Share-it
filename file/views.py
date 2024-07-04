e18199f913903bad062be425a5a939f5eabfe447
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-04 16:45:19 +0200

    refactor: Change file view function to accept file token
    Changed the file function to acceot token and use imported module
    from Django shortcuts to search for an object or return 404

--- file/views.py
+++ file/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from upload.models import UploadFile
 
-def file(request):
-    return render(request, 'file/download.html')
+def file(request, file_token):
+    file = get_object_or_404(UploadFile, download_link=file_token)
+    
+    context = {
+		'file_name': file.file_name,
+		'file_size': file.file_size,
+		'download_link': file.file.url
+	}
+    return render(request, 'file/download.html', context)
 
 def delete(request):
     return render(request, 'file/delete.html')
