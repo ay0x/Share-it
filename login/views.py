6c21f8927c1aa481335448acfb8938e2dd420f2c
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-04 11:58:25 +0200

    feat: Add views for login app
    Added views to handle '/login' request

--- login/views.py
+++ login/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
-def index(request):
-    return render(request, 'pages/login.html') 
+from .forms import LoginForm
+
+@csrf_exempt
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You have successfully logged in!')
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Invalid email or password.')
+                return redirect('login')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'pages/login.html')
