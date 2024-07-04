e63843df05eabbf568345bf6d255da1706687cbe
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-04 16:41:28 +0200

    feat: Add url pattern to capture the file token and route to view function
    Captured token will be used to search and download the right file

--- file/urls.py
+++ file/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('file/', views.file, name='file'),
+    path('file/<str:file_token>', views.file, name='file'),
     path('file/delete/', views.delete, name='delete'),
 ]
