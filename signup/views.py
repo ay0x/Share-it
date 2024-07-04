d97bf74d0c28584292af49abaaf81e3210d5db69
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-04 20:00:11 +0200

    refactor: Generate unique token for secondary ID
    Used custom function to generate token for secondary user identification
    for identifying the uploader

--- signup/views.py
+++ signup/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.contrib import messages
+from shareit.my_utils import generate_token
 
 def signup(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            user.user_unique_gen_id = generate_token(32)
             user.set_password(form.cleaned_data['password'])
             user.save()
             messages.success(request, 'Profile created successfully! Please log in.')
