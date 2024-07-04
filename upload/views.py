42398cbd4281539f6fd803c9c126949cd521b87a
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-04 13:51:22 +0200

    fix: Upload model bug not allowing Null value
    Fixed Upload model to allow guest user upload without User ID

--- upload/views.py
+++ upload/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import UploadFileForm
 from .models import UploadFile
+from signup.models import User
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
+            user_unique_gen_id = request.POST.get('uploader')
+            try:
+                user = User.objects.get(user_unique_gen_id=user_unique_gen_id)
+            except User.DoesNotExist:
+                user = None
+            
             files = request.FILES.getlist('files')
+            
             token_list = []
             file_list = []
             for file in files:
-                uploaded_file = UploadFile(file=file)
+                uploaded_file = UploadFile(file=file, upload_by=user if user else None)
                 uploaded_file.save()
+                print("After save")
                 token_list.append(uploaded_file.download_link)
                 file_list.append({
                     'file_name': uploaded_file.file_name,
