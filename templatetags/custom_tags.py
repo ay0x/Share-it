5069030bf6917b5c4286bdaa9b0708dc695db338
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-04 17:27:04 +0200

    feat: Add custom_tags for templates
    Added custom tags along side official template tags to manipulate
    rendered data

---
+++ templatetags/custom_tags.py
@@ -0,0 +1,29 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def first_two_upper(value):
+    if isinstance(value, str) and len(value) >= 2:
+        return value[:2].upper()
+    return value.upper() if isinstance(value, str) else value
+
+@register.filter
+def first_name_capitalize(value):
+    if isinstance(value, str):
+        first_name = value.split()[0]
+        return first_name.capitalize()
+    return value
+
+@register.simple_tag
+def size_unit(value):
+    if (value < 1024):
+        return f"{value} Bytes"
+    elif (value < 1024**2):
+        return f"{value / 1024:.2f} KB"
+    elif (value < 1024**3):
+        return f"{value / 1024**2:.2f} MB"
+    elif (value < 1024**4):
+        return f"{value / 1024**3:.2f} GB"
+    else:
+        return f"{value / 1024**4:.2f} TB"
