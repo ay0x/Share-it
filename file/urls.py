86ead07d67a7e9c181f6614f2b5785321b917f9b
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-06 11:44:44 +0200

    refactor: Change 'delete' URL pattern to process file tokens

--- file/urls.py
+++ file/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('file/<str:file_token>', views.file, name='file'),
     path('file/user/dl/<str:file_token>', views.user_dl, name='user_dl'),
-    path('file/delete/', views.delete, name='delete'),
+    path('file/delete/<str:file_token>', views.delete, name='delete'),
 ]
