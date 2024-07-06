ec91e9fac300e5cbb7b10efa81d249889878cd0e
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-06 11:09:46 +0200

    refactor: Edit function to initialize full name on the form.
    The profile now dipsplays the profile name by default. And the cancel
    button now redirects to home page

--- account/views.py
+++ account/views.py
@@ -11,21 +11,25 @@ def dashboard(request):
 @login_required(login_url='/login/')
 def profile(request):
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
+        full_name = request.POST.get('full_name')
+        if full_name:
+            request.user.full_name = full_name
+            request.user.save()
             messages.success(request, 'Your profile was successfully updated!')
             return redirect('profile')
         else:
             messages.error(request, 'Please correct the error below.')
-    else:
-        form = ProfileForm(instance=request.user)
-    return render(request, 'account/profile.html', {'form': form})
+
+    return render(request, 'account/profile.html', {'user': request.user})
 
 @login_required(login_url='/login/')
 def history(request):
+    if request.method == 'POST':
+        return redirect('upload_file')
+        
     user_files = request.user.uploadfile_set.all()
     return render(request, 'account/history.html', {'user_files': user_files})
+
 
 @login_required(login_url='/login/')
 def logout(request):
