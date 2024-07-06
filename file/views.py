9acef11d6f341d25e3d52bc446f38585dbadfa1f
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-06 11:18:30 +0200

    refactor: Update views function to restrict unauthorized access to download page
    Only the uploader can delete the file and any user with the delete link

--- file/views.py
+++ file/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from upload.models import UploadFile
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def file(request, file_token):
     file = get_object_or_404(UploadFile, download_link=file_token)
@@ -10,6 +12,31 @@ def file(request, file_token):
 		'download_link': file.file.url
 	}
     return render(request, 'file/download.html', context)
+
+
+@login_required(login_url='/login/')
+def user_dl(request, file_token):
+    file = get_object_or_404(UploadFile, download_link=file_token)
+    
+    # Check if the logged-in user is the uploader
+    if file.upload_by != request.user:
+        context = {
+            'file_name': file.file_name,
+            'file_size': file.file_size,
+            'download_link': file.file.url
+        }
+        messages.error(request, 'You cannot access the restricted part of this file because you are not the uploader')
+        return render(request, 'file/download.html', context)
+    else:
+        context = {
+            'file_name': file.file_name,
+            'file_size': file.file_size,
+            'download_link': file.download_link,
+            'delete_link': file.delete_link
+        }
+        
+        return render(request, 'file/user_download.html', context)
+
 
 def delete(request):
     return render(request, 'file/delete.html')
