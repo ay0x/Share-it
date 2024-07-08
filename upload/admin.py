f4620490b58a58a4e638455f7e3d28eb4c24e4d3
ay0x <85720179+ay0x@users.noreply.github.com>
2024-07-08 14:24:04 +0200

    feat: Add new child model to handle uploaded files information on Admin
    Admin now display more information about an uploaded file

--- upload/admin.py
+++ upload/admin.py
@@ -10,10 +10,22 @@ on `UploadFile` instances via the Django admin panel.
 Imports:
     - admin: Django's module for creating admin interfaces.
     - UploadFile: The model to be registered with the admin site.
+    - timezone: The module for checking expired link.
 """
 
 from django.contrib import admin
 from .models import UploadFile
+from django.utils import timezone
 
+class UploadFileAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'file_size', 'upload_date', 'upload_by', 'expiration_date', 'is_expired')
+    search_fields = ('file_name', 'upload_by__username')
+    list_filter = ('upload_date', 'expiration_date')
+    readonly_fields = ('download_link', 'delete_link')
 
-admin.site.register(UploadFile)
+    def is_expired(self, obj):
+        return obj.expiration_date and obj.expiration_date < timezone.now()
+    is_expired.boolean = True 
+    is_expired.short_description = 'Expired'
+
+admin.site.register(UploadFile, UploadFileAdmin)
